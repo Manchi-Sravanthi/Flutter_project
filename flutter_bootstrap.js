609c0591790fab1b3f19749bd62498c530beec15
sravanthi <sravanthimanchi5@gmail.com>
2026-03-12 14:27:14 +0530

    Remove splash screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3959566159"
+    serviceWorkerVersion: "908718126"
   }
 });
