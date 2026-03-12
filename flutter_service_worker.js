5b58d915d72e158614876a5722c9101a8b90b839
sravanthi <sravanthimanchi5@gmail.com>
2026-03-12 14:20:33 +0530

    Set home as initial route

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e7cf2d121d91a2fa577a17d64122df5d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "85bfca03505a91b10b557253df0c438c",
+"flutter_bootstrap.js": "9fb9b8bdaa868ba5e4469578ba2ebe39",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
